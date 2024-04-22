128280c8e48f7a90efd959c20a3af3fb9b35b68e
Chanop Khonnasee <chanop.k@cimbthai.com>
2024-04-22 09:49:10 +0700

    add pokemon detail

--- src/compoments/PokemonDetail.tsx
+++ src/compoments/PokemonDetail.tsx
@@ -6,27 +6,47 @@ import { PokemonObject } from '../models/PokemonObject'
 
 function PokemonDetail() {
 
-    const { entry_number } = useParams();
+  const { entry_number } = useParams();
 
-    const [data, setData] = useState<PokemonObject>()
+  const [data, setData] = useState<PokemonObject>()
 
-    useEffect(() => {
-      const fetchData = async () => {
-        const result = await axios.get('https://pokeapi.co/api/v2/pokemon/'+entry_number);
-        setData(result.data)
-        console.log(result.data)
-      };
-  
-      fetchData();
-    }, [entry_number]);
-// TODO add detail and style
-    return (
-        <div>
-            <p>Name: {data?.name}</p>
-            {/* <img src={data?.sprites.front_default} alt="Pokemon Image" className="image-responsive"></img> */}
-            <img src={data?.sprites.versions['generation-vi']['x-y'].front_default} alt="Pokemon Image" className="image-responsive"></img>
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('https://pokeapi.co/api/v2/pokemon/' + entry_number);
+      setData(result.data)
+      console.log(result.data)
+    };
+
+    fetchData();
+  }, [entry_number]);
+  // TODO add detail and style
+  return (
+    <div>
+      <div>Name: {data?.name}</div>
+      {/* <img src={data?.sprites.front_default} alt="Pokemon Image" className="image-responsive"></img> */}
+      <img src={data?.sprites.versions['generation-vi']['x-y'].front_default} alt="Pokemon Image" className="image-responsive"></img>
+      <div>Height: {data?.height}</div>
+      <div>Weight: {data?.weight}</div>
+      <div>
+        <div className="row-container">
+          <div>Types: </div>
+          {data?.types.map((val) => (
+            <div key={val.type.name}> {val.type.name} </div>
+          ))}
         </div>
-    )
+      </div>
+      <div>
+      <div className="column-container">
+        <div>Stats:</div>
+        {data?.stats.map((val) => (
+          <div key={val.stat.name}>
+            {val.stat.name} {val.base_stat}
+          </div>
+        ))}
+      </div>
+      </div>
+    </div>
+  )
 }
 
 export default PokemonDetail
